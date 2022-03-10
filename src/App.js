5aa25b30298eec89bc0a52db5c2379daef4fcb18
PeterKampete <peterkampete01@gmail.com>
2022-03-10 20:53:21 +0100

    about to start routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Products, Navbar, Cart } from "./components";
 
 const App = () => {
-  return <div> Hello </div>;
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(cart);
+
+  return (
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Routes>
+          <Route exact path="/"></Route>
+          <Products products={products} onAddToCart={handleAddToCart} />
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
